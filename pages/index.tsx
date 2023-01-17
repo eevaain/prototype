97d32b5d823b4637ce0a30081a6af1fe7e4f3657
Evan Lin <62809897+eevaain@users.noreply.github.com>
2023-01-17 18:27:12 -0500

    uploading my-app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,56 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className="flex flex-col items-center justify-center h-screen overflow-hidden">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Italiana&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        <div className="italiana text-lg pl-44 flex justify-between pt-10 pb-20 min-w-full pr-44">
+          <div className="flex space-x-20 self-center">
+            <div>
+              <img src="Group 10.png" width={80} height={100} />
+            </div>
+            <div className="flex space-x-8">
+              <div>PRICING</div>
+              <div>ABOUT</div>
+              <div>FAQ</div>
+              <div>CAREERS</div>
+            </div>
+          </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <button className="rounded-full gradientButton text-white py-2 px-6">
+            CONTACT
+          </button>
         </div>
-      </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+        <div className="flex pt-5 w-3/4 h-3/4 rounded-full gradientCard">
+          <div className="flex flex-col self-center pl-28 space-y-8">
+            <a className="italiana  text-2xl text-8xl ">Your Dream Website</a>
+            <a className="italiana  text-2xl text-2xl ">
+              Lorem ipsum dolor sit, consectetur <br /> adipiscing elit.{" "}
+            </a>
+            <button className="italiana  rounded-full text-black border-2 border-black text-2xl py-4 w-60">
+              Learn More
+            </button>
+          </div>
+        </div>
 
-export default Home
+        <div className="pt-14">
+          <h1 className="italiana text-6xl font-bold">Pricing</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '2rem',
+      'large': '12px',
+    },
+    minWidth: {
+      '0': '0',
+      '4/10': '40%',
+      '1/2': '50%',
+      '3/4': '75%',
+      '9/10': '90%',
+      'full': '100%',
+     },
+  }
 }
